7302038ca72f4987fbcafb43c0026ce9db3f6482
PRAJYOT <prajyotnarulkar@gmail.com>
2020-03-27 10:30:02 +0530

    serup start script ans heroku port

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const hbs = require('hbs')
 var app = express();
 const fs = require('fs');
 
+const port = process.env.PORT || 3000 ;
+
+
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
 
@@ -63,6 +66,8 @@ app.get('/bad',(req,res)=>{
     })
 });
 
-app.listen(3000,()=>{
-    console.log("Server is ready on port 3000");
+
+
+app.listen(port,()=>{
+    console.log(`Server is ready on port ${port}`);
 });
